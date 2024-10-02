5bc3328c8c1c2a395fe2cacd22a89ed936e49a1b
fanyingfx <fanyingfx@outlook.com>
2024-10-02 13:47:46 +0800

    add first,rest function

--- load_conf.py
+++ load_conf.py
@@ -1,17 +1,24 @@
+#!/usr/bin/env python3
 import toml
 import sys
+import pprint
+
+def first(l):
+    return l[0]
+def rest(l):
+    return l[1:]
 
 def query_config(config,key_list):
     if len(key_list)==1:
-        return config[key_list[0]]
-    return query_config(config[key_list[0]],key_list[1:])
+        return config[first(key_list)]
+    return query_config(config[first(key_list)],rest(key_list))
 
 
 if __name__=="__main__":
     config=toml.load("./computer_config.toml")
     if len(sys.argv)==1:
-        print(config)
-    for arg in sys.argv[1:]:
+        pprint.pp(config)
+    for arg in rest(sys.argv):
         key_list = arg.split('.')
-        print(query_config(config,key_list))
+        pprint.pp(query_config(config,key_list))
 
